1804ad71f2cdbd59fdb70a09f8878634ceaecaf7
Ivan Thiessen <ivant920@gmail.com>
2021-11-30 17:29:13 -0500

    Installed Inquirer, added a bunch of user input prompts to gather data for the generator that hasn't been built yet.

--- index.js
+++ index.js
@@ -1,7 +1,100 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptQuestions = questions => {
+    questions.repository = [];
+    console.log(`
+    ================================
+    Welcome to the README Generator!
+    ================================
+    `);
+    console.log(`
+    Items marked with an Asterisk are required!
+    `)
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?*',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project title!')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'desc',
+            message: 'Please provide a detailed description of your project.*',
+            validate: descInput => {
+                if (descInput) {
+                    return true;
+                } else {
+                    console.log('You need a description!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'link',
+            message: 'Please provide a link to your deployed website if applicable.'
+        },
+        {
+            type: 'checkbox',
+            name: 'lang',
+            message: 'What did you build this project with? (Check all that apply)',
+            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+        {
+            type: 'confirm',
+            name: 'confirmInstall',
+            message: 'Add installation instructions?',
+            default: true
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'Write out your Installation instructions here.',
+            when: ({confirmInstall}) => {
+                if (confirmInstall) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmUse',
+            message: 'Add usage instructions?',
+            default: true
+        },
+        {
+            type: 'input',
+            name: 'use',
+            message: 'Write out your Usage instructions here.',
+            when: ({ confirmUse }) => {
+                if (confirmUse) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmLicense',
+            message: 'Add a license so others can use your work?',
+            default: true
+        },
+    ])
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -10,4 +103,4 @@ function writeToFile(fileName, data) {}
 function init() {}
 
 // Function call to initialize app
-init();
+promptQuestions();
